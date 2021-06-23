74b2ded856d9a3e1a2cb049ae0c47fa324cd5a5f
Tuong Vy <58203954+KhanhTuong1211@users.noreply.github.com>
2021-06-23 14:01:15 +0700

    update controller

---
+++ sever/controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class Request
+{
+    public $reId;
+    public $pName;
+    public $cuId;
+    public $pLanguage;
+    public $category;
+    public $imagesLink;
+    public $videoLink;
+    public $price;
+    public $info;
+    public $tRequest;
+    public $private;
+}
+?>

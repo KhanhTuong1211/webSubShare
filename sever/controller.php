3f092fe0b4d4c5715b0b6e50bf84481c91eb08de
Tuong Vy <58203954+KhanhTuong1211@users.noreply.github.com>
2021-06-23 14:02:47 +0700

    edit

--- sever/controller.php
+++ sever/controller.php
@@ -14,4 +14,39 @@ class Request
     public $tRequest;
     public $private;
 }
+
+function deleteShare($reId){
+    //Get data from database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "subshare";
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn -> set_charset("utf8");
+
+    //Check connection
+    if($conn ->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT share.pName as pName, customer.nName as nName, share.pLanguage as pLanguage, share.tShare as tShare 
+    FROM share, customer WHERE pName LIKE '%$keyword%' LIMIT 100;";
+    // echo $sql;
+    // die();
+    
+    $result = $conn->query($sql);
+    // var_dump ($result);
+    // die();
+    if ($result->num_rows > 0) {
+        // Convert $result to json format
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        // var_dump($data);
+        // die();
+        echo json_encode($data);
+      } else {
+        echo "{result: \"No result found\"}";
+      }
+    $conn->close();
+  }
+
 ?>
